0d47df41f4788c40488b919bf72e394753a05c82
mehul.ghosal@gmail.com <mehul ghosal>
2017-03-09 17:15:14 +0000

    some more setting up

---
+++ House.java
@@ -0,0 +1,13 @@
+public class House{
+
+	private String name;
+	private double xPos; //streets (250)
+	private double yPos; //avenues (50)
+
+	public House(String name){
+		this.name = name;
+		if(name.equals())
+	}
+
+
+}
---
+++ Street.java
@@ -0,0 +1,11 @@
+public class Street{
+
+	private House[][] street = new House[2000]; //every street has 20 houses
+	public 
+
+	public Street(){
+
+	}
+
+
+}
