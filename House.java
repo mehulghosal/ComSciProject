600f01ff9187c3e6ebe4ce2575efb5c7d0671253
mehul.ghosal@gmail.com <mehul ghosal>
2017-03-10 04:22:28 +0000

    3/9 get methods in house and constructor in street

--- House.java
+++ House.java
@@ -1,12 +1,55 @@
 public class House{
 
 	private String name;
-	private double xPos; //streets (250)
-	private double yPos; //avenues (50)
-
-	public House(String name){
+	private double yPos; 
+	private double street;
+	
+	public House(String name, double street){
 		this.name = name;
-		if(name.equals())
+		if(name.equals("A") || name.equals("AA")){
+			yPos = .1;
+		}
+		else if(name.equals("B") || name.equals("BB")){
+			yPos = .2;
+		}
+		else if(name.equals("C") || name.equals("CC")){
+			yPos = .3;
+		}
+		else if(name.equals("D") || name.equals("DD")){
+			yPos = .4;
+		}
+		else if(name.equals("E") || name.equals("EE")){
+			yPos = .5;
+		}
+		else if(name.equals("F") || name.equals("FF")){
+			yPos = .6;
+		}
+		else if(name.equals("G") || name.equals("GG")){
+			yPos = .7;
+		}
+		else if(name.equals("H") || name.equals("HH")){
+			yPos = .8;
+		}
+		else if(name.equals("I") || name.equals("II")){
+			yPos = .9;
+		}
+		else if(name.equals("J") || name.equals("JJ")){
+			yPos = 1.0;
+		}
+		
+		this.street = street;
+	}
+	
+	public double getStreet(){
+		return street;
+	}
+	
+	public double getYPos(){
+		return yPos;
+	}
+	
+	public String getName(){
+		return name;
 	}
 
 
--- Street.java
+++ Street.java
@@ -1,10 +1,16 @@
 public class Street{
 
-	private House[][] street = new House[2000]; //every street has 20 houses
-	public 
+	private House[] street = new House[2000]; //every street has 20 houses
+	private double streetNumber;
+//	public 
 
-	public Street(){
-
+	public Street(double streetNumber){
+		this.streetNumber = streetNumber;
+/*		for(int i = 0; i<2000; i++){
+			for (double j =0; j<20; j+= .1){
+				street[i] = new House();
+			}
+		}*/
 	}
 
 
