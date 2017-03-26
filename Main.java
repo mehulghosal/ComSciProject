fbb62c225042d07e167ea07c450f184c7c2125b7
mehul.ghosal@gmail.com <mehul ghosal>
2017-03-26 21:52:33 +0100

    more additions

--- Main.java
+++ Main.java
@@ -23,9 +23,10 @@ public class Main{
 		}
 		
 		int cycleNumber = Integer.parseInt(records.get(0)); //this is the first input -- the cycle number
+		//System.out.println(cycleNumber + " " + records.get(1));
 		
-		//why is this line producing an error?
-		int numberOfInputs = Integer.parseInt(records.get(1)); //this is the total number of inputs
+		//this is giving us an error because it is not a striaght number, it has a comma(not 2350, it is 2,350)
+		int numberOfInputs = Integer.parseInt(records.get(1).replaceAll("[^0-9]", "")); //this is the total number of inputs
 		
 		//this is all to set up the array of houses that need to be visited
 		House[] inputs = new House[numberOfInputs];
@@ -42,34 +43,34 @@ public class Main{
 			holderStreet = Integer.parseInt(holderArray[0].replaceAll("[^0-9]", ""));
 			holderAve = Integer.parseInt(holderArray[1].replaceAll("[^0-9]", ""));
 			
-			if(holderArray[3].equals("A") || holderArray[3].equals("AA")){
+			if(holderArray[2].equals("A") || holderArray[2].equals("AA")){
 				houseAddress = 0;
 			}
-			else if(holderArray[3].equals("B") || holderArray[3].equals("BB")){
+			else if(holderArray[2].equals("B") || holderArray[2].equals("BB")){
 				houseAddress = .1;
 			}
-			else if(holderArray[3].equals("C") || holderArray[3].equals("CC")){
+			else if(holderArray[2].equals("C") || holderArray[2].equals("CC")){
 				houseAddress = .2;
 			}
-			else if(holderArray[3].equals("D") || holderArray[3].equals("DD")){
-				houseAddress = .3;
+			else if(holderArray[2].equals("D") || holderArray[2].equals("DD")){
+				houseAddress = .2;
 			}
-			else if(holderArray[3].equals("E") || holderArray[3].equals("EE")){
+			else if(holderArray[2].equals("E") || holderArray[2].equals("EE")){
 				houseAddress = .4;
 			}
-			else if(holderArray[3].equals("F") || holderArray[3].equals("FF")){
+			else if(holderArray[2].equals("F") || holderArray[2].equals("FF")){
 				houseAddress = .5;
 			}
-			else if(holderArray[3].equals("G") || holderArray[3].equals("GG")){
+			else if(holderArray[2].equals("G") || holderArray[2].equals("GG")){
 				houseAddress = .6;
 			}
-			else if(holderArray[3].equals("H") || holderArray[3].equals("HH")){
+			else if(holderArray[2].equals("H") || holderArray[2].equals("HH")){
 				houseAddress = .7;
 			}
-			else if(holderArray[3].equals("I") || holderArray[3].equals("II")){
+			else if(holderArray[2].equals("I") || holderArray[2].equals("II")){
 				houseAddress = .8;
 			}
-			else if(holderArray[3].equals("JJ") || holderArray[3].equals("JJ")){
+			else if(holderArray[2].equals("JJ") || holderArray[2].equals("JJ")){
 				houseAddress = .9;
 			}
 			
