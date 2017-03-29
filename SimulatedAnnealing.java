9b7ec3b3e48906ac24773a52047ac2f513939a09
Kevin Wang <kevinshiyuwang@gmail.com>
2017-03-29 13:54:19 -0400

    this is a test

--- SimulatedAnnealing.java
+++ SimulatedAnnealing.java
@@ -5,7 +5,7 @@ public class SimulatedAnnealing{
 	private double temperature = 10000; //temperature is basically a counter, but counts down -- setting an intial temperature
 	private double coolingRate = 0.003; //this is the cooling rate -- after every run through, multiply temperature by (1-coolingrate)
 	
-	//now what
+	//test
 	
 	
 }
