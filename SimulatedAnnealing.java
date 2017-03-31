a415f4e3a6ffb685f6f55b528b492cf624bbe577
mehul.ghosal@gmail.com <mehul ghosal>
2017-03-31 23:51:19 +0100

    i tried to do something new

--- SimulatedAnnealing.java
+++ SimulatedAnnealing.java
@@ -5,7 +5,16 @@ public class SimulatedAnnealing{
 	private double temperature = 10000; //temperature is basically a counter, but counts down -- setting an intial temperature
 	private double coolingRate = 0.003; //this is the cooling rate -- after every run through, multiply temperature by (1-coolingrate)
 	
-	//test
+	//this is for the 
+	public static double acceptanceProbability(int currentDistance, int newDistance, double temperature) {
+		// If the new solution is better, accept it
+		if (newDistance < currentDistance) {
+			return 1.0;
+		}
+		// If the new solution is worse, calculate an acceptance probability
+		return Math.exp((currentDistance - newDistance) / temperature);
+	}
+	
 	
 	
 }
