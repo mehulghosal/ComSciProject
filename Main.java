c2bb808895560e89b0e1389a04561ec814fe4040
Kevin Wang <kevinshiyuwang@gmail.com>
2017-04-25 12:03:27 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -27,7 +27,7 @@ public class Main{
 		
 		int cycleNumber = Integer.parseInt(records.get(0)); //this is the first input -- the cycle number
 		
-		int numberOfInputs = Integer.parseInt(records.get(1).replaceAll("[^0-9]", "")) + 2; //this is the total number of inputs
+		int numberOfInputs = Integer.parseInt(records.get(1).replaceAll("[^0-9]", "")) + 4; //this is the total number of inputs
 		
 		
 		//this is all to set up the array of houses that need to be visited
@@ -36,6 +36,8 @@ public class Main{
 		int holderStreet;
 		int holderAve;
 		double houseAddress = 0;
+		
+		inputs[0] = new House(125,22); //start at distribution center
 		
 		for(int i = 0; i < records.size() - 6; i++){
 			
@@ -74,13 +76,14 @@ public class Main{
 			}
 			
 						
-			inputs[i] = new House(holderStreet, holderAve + houseAddress);
+			inputs[i+1] = new House(holderStreet, holderAve + houseAddress);
 			
 		}
 		
 		//Bart and lisa
-		inputs[inputs.length - 2] = new House(2, 3);//bart
-		inputs[inputs.length -1] = new House(149, 33);//lisa
+		inputs[inputs.length - 3] = new House(2, 3);//bart
+		inputs[inputs.length -2] = new House(149, 33);//lisa
+		inputs[inputs.length -1] = new House(125,22); //end at distr. center
 		
 		double bartInputs = Double.parseDouble(records.get(records.size() - 3));
 		double lisaInputs = Double.parseDouble(records.get(records.size() - 1));
@@ -133,9 +136,7 @@ public class Main{
 		System.out.println("original distance: "+ distOriginal);
 		System.out.println("final distance:" + distFinal);
 		System.out.println("The best distance was " + finalfinal);
-        
-        double xz = SimulatedAnnealing.getDistance();
-        SimulatedAnnealing.setDistance(SimulatedAnnealing.getDistance()+)
+
 		
 
 		
