75d89e734ffdfe30e49c5d25bab44cd52bfa834a
Kevin Wang <kevinshiyuwang@gmail.com>
2017-04-26 19:41:54 -0400

    Update SimulatedAnnealing.java

--- SimulatedAnnealing.java
+++ SimulatedAnnealing.java
@@ -50,7 +50,7 @@ public class SimulatedAnnealing{
 		route = new House[original.length+2]; 
 		route[0] = new House(125,22);
 		ArrayList<Integer> randomChecker = new ArrayList<Integer>();
-		for(int i = 0; i<original.length-1; i++) {
+		for(int i = 0; i<original.length; i++) {
 			int x = (int)(Math.random()*(original.length));
 			if(randomChecker.contains(x)) {
 				i--;
