c2eaec629175d8da21a2cd7c4a8ea67db2f417aa
Kevin Wang <kevinshiyuwang@gmail.com>
2017-04-27 19:44:56 -0400

    Update Truck.java

--- Truck.java
+++ Truck.java
@@ -1,41 +1,51 @@
 
 public class Truck {
 	
-	private double time = 0; //cumulative -- add on to total and reset it when restarting
-	private double x = 125 * 200; //for moving the truck, this is the street its on, starts at distribution center
-	private double y = 22 * 1000; //for moving the truck, this is the ave its on, starts at distribution center
-	private House currentLocation;
+	private double time = 0;
+	private double distance = 0;
+	private double cost = 0;
+	private House[] truckRoute;
+	private int bartInputs;
+	private int lisaInputs;
+	private boolean bart;
+	private boolean lisa;
+	private boolean rental;
+	private int houses;
 	
-	public void setTime(double time){
-		this.time = time;
+	public Truck(House[] route, int numHouses, int bartIn, int lisaIn, boolean b, boolean l, boolean r) {
+		truckRoute = route;
+		bartInputs = bartIn;
+		lisaInputs = lisaIn;
+		bart = b;
+		lisa = l;
+		rental = r;
+		distance = truckRoute.getDistance();
 	}
 	
-	public double getTime(){
-		return time;
+	public double calcTime() {
+		time+= (distance/5000)/(100.0/3);
+		time+= houses*60;
+		if(bart) {
+			time+=(bartIn*30);
+		}
+		if(lisa) {
+			time+=(lisa*30);
+		}
+	}
+	public double calcCost() {
+		cost+=(distance/1000)+((int)(distance/5000))*10;
+		if(rental) {
+			cost+=15000
+		}
+		for(int i = 1; i<=(time/3600); i++) {
+			if(i<=8) {
+				cost+=30;
+			}
+			else {
+				cost+=45;
+			}
+		}
 	}
 	
-	public void setX(double x){
-		this.x = x;
-	}
-	
-	public void setY(double y){
-		this.y = y;
-	}
-	
-	public double getX(){
-		return x;
-	}
-	
-	public double getY(){
-		return y;
-	}
-	
-	public void move(House from){
-		currentLocation = from;
-	}
-	
-	public void honk(){
-		System.out.println("Beep Beep");
-	}
 	
 }
