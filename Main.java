ed05ee47b24e8d2e7ae8fa693a7f03d28479bf2c
Kevin Wang <kevinshiyuwang@gmail.com>
2017-05-06 22:09:52 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -113,6 +113,7 @@ public class Main{
 			
 			for(int i = 0; i<inputs.length; i++) {
 				int toAve = (int)((inputs[i].getY())/1000);
+				System.out.println(toAve);
 				int toTruck = aveHolder[toAve];
 				trucks[toTruck].addHouse(inputs[i]);
 			}
